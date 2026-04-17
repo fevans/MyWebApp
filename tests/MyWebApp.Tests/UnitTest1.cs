9c89a2628faaa42a36b6a668d8d5061358d43f0b
Francis Evans <francis.evans@gmail.com>
2026-04-17 12:46:18 +0100

    Add GitHub Actions CI workflow

---
+++ tests/MyWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace MyWebApp.Tests;
+
+public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public ProgramTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetRootEndpoint_ReturnsHelloWorld()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Hello World!", content);
+    }
+
+    [Fact]
+    public async Task GetRootEndpoint_Returns200Ok()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+    }
+}
